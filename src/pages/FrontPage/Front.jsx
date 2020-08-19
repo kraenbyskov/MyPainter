61297e943071aad054d370b7635addffe912c977
kraenbyskov <kraenbyskov@gmail.com>
2020-08-19 09:32:18 +0200

    We can navigate again

--- src/pages/FrontPage/Front.jsx
+++ src/pages/FrontPage/Front.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "./Front.module.scss";
 const Frontpage = () => {
   return (
-    <div>
+    <div className={style.Front}>
       <h1>Welcome to MyPainter 0.2 Alpa</h1>
-      Login to site
+      <p>Login to site go to Artboards</p>
+      <p>
+        <Link to={`/VieweArtboard`}>or just click here</Link>
+      </p>
     </div>
   );
 };
